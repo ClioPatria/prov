28e3e985b0a3d4abc6a06698e6988d9047ffac97
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-01-23 15:33:24 +0100

    added support for prov, stop using opmv

--- config-available/opmv.pl
+++
@@ -1,6 +0,0 @@
-:- module(conf_opmv, []).
-
-/** <module> Schema and viz. supporting the Open Provenance Model
-*/
-
-:- use_module(library(opmv_schema)).
---
+++ lib/prov_schema.pl
@@ -0,0 +1,13 @@
+:- module(prov_schema, []).
+:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf_library)).
+
+/** <module> Provide PROV schema, namespace and visualization hooks.
+
+This module provides the PROV schema and the prefix =prov= for use in
+Prolog.
+*/
+
+% :- rdf_register_ns(prov,  'http://www.w3..org/net/opmv/ns#').
+:- rdf_attach_library(prov(rdf)).
+:- rdf_load_library(prov).
