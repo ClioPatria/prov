d073427b7d55a9f3935183d61a8ec69bd3d52956
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2017-04-21 13:51:07 +0200

    ADDED: reusable prov predicates from Talk of Europe project- experimental

--- lib/prov_schema.pl
+++ lib/prov_schema.pl
@@ -1,5 +1,22 @@
-:- module(prov_schema, []).
+:- module(prov_schema, [
+              prov_init/1,
+              default_provenance_graph/1,
+              log_start_activity/3,
+              log_end_activity/3,
+              log_entity_use/2,
+              log_entity_create/2,
+              prov_uri/3,
+              xsd_now/1
+          ]).
 :- use_module(library(semweb/rdf_library)).
+:- use_module(library(semweb/rdf11)).
+:- use_module(library(semweb/rdf_persistency)).
+
+:- use_module(library(git)).
+:- use_module(library(option)).
+:- use_module(library(socket)).
+:- use_module(library(uri)).
+:- use_module(library(version)).
 
 /** <module> Provide PROV schema, namespace and visualization hooks.
 
@@ -7,6 +24,192 @@ This module provides the PROV schema and the prefix =prov= for use in
 Prolog.
 */
 
-% :- rdf_register_ns(prov,  'http://www.w3..org/net/opmv/ns#').
+:- rdf_register_ns(provx,  'http://cliopatria.swi-prolog.org/ns/prov#').
+:- rdf_register_ns(doap,   'http://usefulinc.com/ns/doap#').
+
 :- rdf_attach_library(prov(rdf)).
 :- rdf_load_library(prov).
+
+:- dynamic
+    current_prov_uri/2.
+
+:- rdf_meta
+    default_provenance_graph(r),
+    current_prov_uri(r,r).
+
+default_provenance_graph(provbundle).
+
+%!  prov_init(+Options) is det.
+%
+%   Clear current_prov_uri cached assertions
+%   and initialize prov bundle
+prov_init(Options) :-
+    retractall(current_prov_uri(_,_)),
+    default_provenance_graph(DefaultBundle),
+    option(prov(ProvBundle), Options, DefaultBundle),
+    rdf_unload_graph(ProvBundle),
+    rdf_persistency(ProvBundle, false),
+    prov_uri(ProvBundle, program(_Program), Options),
+    prov_uri(ProvBundle, person(_Person), Options).
+
+%!  prov_uri(+Graph, -URI, +Options) is det.
+%
+%   URI is unified with the URI associated with the current
+%   SoftwareAgent (program) or Person in the prov bundle Graph.
+prov_uri(Graph, UriTerm, _Options) :-
+    current_prov_uri(Graph, UriTerm),!.
+
+prov_uri(Graph, program(Program), Options) :-
+    prov_program(Graph, Program, Options), !.
+
+prov_uri(Graph, person(Person), Options) :-
+    prov_person(Graph, Person, Options), !.
+
+
+prov_program(Graph, Program, Options)  :-
+    (   current_prolog_flag(version_git, PL_version)
+    ->  true
+    ;   current_prolog_flag(version, PL_version)
+    ),
+    current_prolog_flag(executable, PrologExec),
+    uri_file_name(PrologExecURL, PrologExec),
+    working_directory(CWD,CWD),
+    uri_file_name(CWDF,CWD),
+    gethostname(LocalHost),
+    findall(M-U-V-F,
+            (   git_module_property(M, home_url(U)),
+                git_module_property(M, version(V)),
+                git_module_property(M, directory(D)),
+                uri_file_name(F, D)
+            ),
+            MUVs
+           ),
+    Prolog = 'swi-prolog'-'http://www.swi-prolog.org'-PL_version-PrologExecURL,
+    AllModules = [Prolog|MUVs],
+    sort(AllModules, SortedModules),
+    variant_sha1(SortedModules, Hash),
+    rdf_global_id(provx:Hash, Program),
+    assert(current_prov_uri(Graph, program(Program))),
+    option(program_label(Label), Options, 'Local ClioPatria instance'@en),
+    rdf_assert(Program, rdfs:label, Label, Graph),
+    rdf_assert(Program, rdf:type,   prov:'SoftwareAgent', Graph),
+    rdf_assert(Program, provx:cwd, CWDF, Graph),
+    rdf_assert(Program, provx:host, LocalHost^^xsd:string, Graph),
+    forall(member(M-U-V-D, SortedModules),
+           (   rdf_create_bnode(B),
+               rdf_assert(Program, provx:component, B, Graph),
+               rdf_assert(B, doap:revision, V@en, Graph),
+               rdf_assert(B, doap:name, M@en, Graph),
+               rdf_assert(B, rdfs:seeAlso, D@en, Graph),
+               rdf_assert(B, doap:homepage, U@en, Graph)
+           )
+          ),
+    !.
+
+prov_person(Graph, Person, Options) :-
+    default_user_name(DefaultUserName),
+    option(user(UserName), Options, DefaultUserName),
+    variant_sha1(UserName, Hash),
+    rdf_global_id(provx:Hash, DefaultPerson),
+    option(person(Person), Options, DefaultPerson),
+    rdf_assert(Person, foaf:name, UserName@en, Graph),
+    rdf_assert(Person, rdf:type, prov:'Person', Graph),
+    assert(current_prov_uri(Graph, person(Person))).
+
+default_user_name(UserName) :-
+    git(['config','--get','user.name'], [output(Codes)]),
+    atom_codes(Atom, Codes),
+    normalize_space(atom(UserName), Atom),
+    !.
+
+
+xsd_now(TimeStamp) :-
+    get_time(Time),
+    xsd_timestamp(Time, TimeStamp).
+
+xsd_timestamp(Time, TimeStamp) :-
+    stamp_date_time(Time, Date, 'UTC'),
+    format_time(atom(TimeStamp), '%FT%T%:z', Date, posix).
+
+log_start_activity(Activity, ProvBundle, Options) :-
+    default_provenance_graph(DefaultBundle),
+    option(label(Label), Options),
+    option(prov(ProvBundle), Options, DefaultBundle),
+    prov_uri(ProvBundle, program(Program), Options),
+    prov_uri(ProvBundle, person(Person), Options),
+    xsd_now(TimeStamp),
+    rdf_create_bnode(Activity),
+    rdf_assert(Activity, rdf:type, prov:'Activity', ProvBundle),
+    rdf_assert(Activity, rdfs:label, Label@en, ProvBundle),
+    rdf_assert(Activity, prov:startedAtTime, TimeStamp^^xsd:dateTime, ProvBundle),
+    rdf_assert(Activity, prov:wasAssociatedWith, Person, ProvBundle),
+    rdf_assert(Activity, prov:wasAssociatedWith, Program, ProvBundle).
+
+log_end_activity(Activity, ProvBundle, _Options) :-
+    xsd_now(TimeStamp),
+    rdf_assert(Activity, prov:endedAtTime, TimeStamp^^xsd:dateTime, ProvBundle).
+
+log_entity_use(Spec, Options) :-
+    default_provenance_graph(DefaultBundle),
+    option(prov(ProvBundle), Options, DefaultBundle),
+    option(activity(Activity), Options),
+    spec_entity_file(Spec, Entity, File),
+    rdf_assert(Entity, rdf:type, prov:'Entity', ProvBundle),
+    rdf_assert(Activity, prov:used, Entity, ProvBundle),
+    (   access_file(File, read)
+    ->  size_file(File, Size),
+        time_file(File, Time),
+        xsd_timestamp(Time, Stamp),
+        rdf_assert(Entity, provx:file_size, Size^^xsd:integer, ProvBundle),
+        rdf_assert(Entity, prov:generatedAtTime, Stamp^^xsd:dateTime, ProvBundle)
+    ;   true
+    ).
+
+spec_entity_file(Spec, Entity, File) :-
+    uri_is_global(Spec),
+    Spec = Entity,
+    uri_file_name(Entity, File),
+    !.
+
+spec_entity_file(Spec, Entity, false) :-
+    uri_is_global(Spec),
+    Spec = Entity,
+    !.
+
+spec_entity_file(Spec, Entity, File) :-
+    uri_file_name(Entity, Spec),
+    Spec = File,
+    !.
+
+log_entity_create(File, Options) :-
+    option(activity(Activity), Options),
+    option(prov(ProvBundle), Options),
+    option(graph(Graph), Options, none),
+    (   uri_is_global(File)
+    ->  Entity = File
+    ;   uri_file_name(Entity, File)
+    ),
+    xsd_now(TimeStamp),
+    rdf_assert(Entity, rdf:type, prov:'Entity', ProvBundle),
+    rdf_assert(Entity, prov:generatedAtTime, TimeStamp^^xsd:dateTime,  ProvBundle),
+    rdf_assert(Entity, prov:wasGeneratedBy, Activity, ProvBundle),
+    log_entity_graph_properties(Entity, Graph, ProvBundle).
+
+log_entity_graph_properties(Entity, Graph, ProvBundle) :-
+    forall(rdf_graph_property(Graph, Property),
+           (   Property =.. [ Local, LValue ],
+               (   Local = triples
+               ->  rdf_global_id(void:Local, Pred)
+               ;   Local = source_last_modified
+               ->  rdf_global_id(dcterms:modified, Pred)
+               ;   Local = source
+               ->  rdf_global_id(prov:wasDerivedFrom, Pred)
+               ;   rdf_global_id(provx:Local, Pred)
+               ),
+               (   Local = source_last_modified
+               ->  xsd_timestamp(LValue, Value)
+               ;   Value = LValue
+               ),
+               rdf_assert(Entity, Pred, Value, ProvBundle)
+           )
+          ).
